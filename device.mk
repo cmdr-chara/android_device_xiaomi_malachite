196f401c2dc7228bfecbb9ad79e4fd9296aced88
Wang Han <416810799@qq.com>
2019-05-13 20:33:41 +0800

    malachite: Set AAPT configuration
    Change-Id: I0aa83727943ac59069f1d27496a2a1b5855cf607

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # Virtual A/B
 PRODUCT_PACKAGES += \
     checkpoint_gc \
