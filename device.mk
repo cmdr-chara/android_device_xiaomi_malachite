9ad0f9b19f3502c6d38a420273a13ca63a08a47e
Bruno Martins <bgcngm@gmail.com>
2023-07-20 14:33:30 +0100

    malachite: overlay: Enable smooth display with 120Hz
    There's no point to keep using custom pref to set minimum refresh rate
    when AOSP smooth display feature behaves in the exact same way in
    our case.
    
    Change-Id: I497918a9c9687443a9438fb6291769f6e51d9379

--- device.mk
+++ device.mk
@@ -203,7 +203,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
-    FrameworkResOverlayMalachite
+    FrameworkResOverlayMalachite \
+    SettingsResOverlayMalachite
 
 # Power
 PRODUCT_PACKAGES += \
