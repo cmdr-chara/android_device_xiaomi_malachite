1b0238ce547d817b571588dc4dabb32cbd19c2eb
Michael Bestas <mkbestas@lineageos.org>
2024-03-06 12:51:55 +0000

    malachite: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- device.mk
+++ device.mk
@@ -220,6 +220,15 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
+# Lineage Health
+$(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/input_suspend)
+$(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
+$(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
+$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+
+PRODUCT_PACKAGES += \
+    vendor.lineage.health-service.default
+
 # Media
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
