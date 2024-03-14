282841d8203eefe2f402b2fb8f875e9079caf4a3
Arian <arian.kulmer@web.de>
2024-03-14 13:46:02 +0100

    malachite: udfps: Drop unnecessary setFingerDown on cancel
    HBM is usually not on when it gets cancelled
    
    Change-Id: I6c2c4836bf9dad627d4f97283b0fe8d2c398908a

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -195,7 +195,6 @@ class XiaomiMalachiteUdfpsHander : public UdfpsHandler {
 
     void cancel() {
         LOG(INFO) << __func__;
-        setFingerDown(false);
         setFodStatus(FOD_STATUS_OFF);
     }
 
