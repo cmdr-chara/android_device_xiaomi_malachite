aeadffc495d5bbfcbafa6669b7febd0362b9974d
bengris32 <bengris32@protonmail.ch>
2024-03-24 15:17:34 +0000

    malachite: Configure default PowerOffAlarm driver
    * Set rtc as the default (and only) driver since this is the only
      supported one on 5.10+ devices.
    
    Change-Id: Iccc5627b5947a0c2cebb8ea706a2af731dfcbf42
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- device.mk
+++ device.mk
@@ -348,6 +348,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageApertureOverlayMalachite \
     LineageDialerMalachite \
+    LineagePowerOffAlarmOverlayMalachite \
     LineageSDKResMalachite \
     LineageSystemUIOverlayMalachite
 
