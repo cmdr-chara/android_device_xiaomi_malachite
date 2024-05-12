8d7c1ed2ff7c9dcd509ea5e04a2149cfb033dd72
Michael Bestas <mkbestas@lineageos.org>
2024-05-12 06:33:38 +0300

    malachite: Remove unused build description override
    Change-Id: I3d53f1cc5d4711403b53b723b95a175e6220702c

--- lineage_malachite.mk
+++ lineage_malachite.mk
@@ -23,7 +23,6 @@ PRODUCT_SYSTEM_NAME := malachite_global
 PRODUCT_SYSTEM_DEVICE := malachite
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="malachite_global-user 15 UP1A.231005.007 OS2.0.3.0.VOOMIXM release-keys" \
     BuildFingerprint=Redmi/malachite_global/malachite:15/UP1A.231005.007/OS2.0.3.0.VOOMIXM:user/release-keys \
     SystemModel=$(PRODUCT_SYSTEM_DEVICE) \
     SystemName=$(PRODUCT_SYSTEM_NAME) \
