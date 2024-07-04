38ff4a62e792f277580b8359a16abf11b2e355f6
Bruno Martins <bgcngm@gmail.com>
2024-07-04 10:41:05 +0530

    malachite: Patch libs for missing GraphicBufferMapper symbols
    Change-Id: I1e79f01710433ceafb2a4b296a591cf910ee649f

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,9 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/hw/android.hardware.security.keymint@3.0-service.mitee': blob_fixup()
         .replace_needed('android.hardware.security.keymint-V3-ndk.so', 'android.hardware.security.keymint-V4-ndk.so'),
 
+    'vendor/lib64/hw/vendor.mediatek.hardware.pq_aidl-impl.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+
     ('odm/lib64/camera/plugins/com.xiaomi.plugin.capdepth.so', 'vendor/lib64/libalNN.so', 'vendor/lib64/libmiphone_preview_bokeh.so', 'vendor/lib64/libmiphone_preview_mdbokeh.so'): blob_fixup()
         .replace_needed('libomp.so', 'libomp_vendor.so'),
 
