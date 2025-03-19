527e33747a4d44d5dc4f12b9ad3601719061ff0e
Sarthak Roy <sarthakroy2002@gmail.com>
2025-03-19 07:32:41 +0530

    malachite: sepolicy: Allow init to bind mount on system/product
    * avc:  denied  { mounton } for  uid=0 path="/product/etc/permissions/android.hardware.telephony.euicc.xml" dev="dm-4" ino=485 scontext=u:r:init:s0 tcontext=u:object_r:system_file:s0 tclass=file permissive=0
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I8ceed63a329b983f62446b8a75d8cf095ed3c18c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -146,6 +146,7 @@ BOOT_SECURITY_PATCH := 2025-01-01
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # vendor_boot image
 AB_OTA_PARTITIONS += \
