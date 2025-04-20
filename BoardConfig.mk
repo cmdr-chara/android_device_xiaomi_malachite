5247bffa3062884f2a0a700c5b45c877ceaf6fa1
claxten10 <claxten10@gmail.com>
2025-04-20 19:42:44 +0200

    malachite: Initial skeleton
    Signed-off-by: claxten10 <claxten10@gmail.com>

---
+++ BoardConfig.mk
@@ -0,0 +1,9 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/malachite
+
+# Inherit the proprietary files
+include vendor/xiaomi/malachite/BoardConfigVendor.mk
