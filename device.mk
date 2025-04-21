6c8320c4e4459d91d1324fd1ba45d26cb7119fc1
claxten10 <claxten10@gmail.com>
2025-04-21 13:05:34 +0200

    malachite: Add `hardware/xiaomi` to soong namespaces
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -21,7 +21,8 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/mediatek
+    hardware/mediatek \
+    hardware/xiaomi
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
