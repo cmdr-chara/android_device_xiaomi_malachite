c6a563cbbabe8599479e1621de6a092967585588
claxten10 <claxten10@gmail.com>
2025-04-21 13:05:00 +0200

    malachite: Add `hardware/mediatek` to soong namespaces
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -20,7 +20,8 @@ PRODUCT_SHIPPING_API_LEVEL := 34
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
