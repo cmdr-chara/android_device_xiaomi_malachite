4c0a87bacc2470cba84b6904b34d86a3b2651c2d
claxten10 <claxten10@gmail.com>
2025-04-21 12:43:51 +0200

    malachite: Define the recovery pixel format as BGRA_8888
    * The BGRA 32-bit RGB format is stored in memory as 8 blue bits, 8 green bits, 8 red bits, and 8 alpha bits.
    
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -109,6 +109,9 @@ TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
 # Platform
 TARGET_BOARD_PLATFORM := mt6878
 
+# Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+
 # vendor_boot image
 AB_OTA_PARTITIONS += \
     vendor_boot
