61e8c4a5cece044a99f958f5ea2f24d85bdea738
claxten10 <claxten10@gmail.com>
2025-05-04 15:06:13 +0000

    malachite: Fix Neural Networks stack
    * Xiaomi did not ship an init nor vintf for the neural network stack????
    * Kang init from tetris and add a vintf entry
    * Kang nnapi_powerhal.json from tetris
    * Kang NNAPI props from tetris
    
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -205,6 +205,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/media,$(TARGET_COPY_OUT_VENDOR)/etc)
 
+# Neural Networks
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/nnapi_powerhal.json:$(TARGET_COPY_OUT_VENDOR)/etc/nnapi_powerhal.json
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
