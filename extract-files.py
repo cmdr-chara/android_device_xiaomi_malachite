a1fdd4e62a43a4b15caf6aa13358c9bb7b6e0ebe
claxten10 <claxten10@gmail.com>
2025-05-04 21:41:11 +0000

    malachite: Uprev graphics.allocator-V1-ndk to V2-ndk
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -43,7 +43,8 @@ blob_fixups: blob_fixups_user_type = {
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V3-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V4-ndk.so',
      'vendor/lib64/vendor.mediatek.hardware.pq_aidl-V7-ndk.so'): blob_fixup()
-        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so'),
+        .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V6-ndk.so')
+        .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
