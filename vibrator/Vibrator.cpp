ff6770e03c619aa0602da671c0aba490d4acb3ac
claxten10 <claxten10@gmail.com>
2025-07-08 19:55:32 +0000

    malachite: vibrator: Add support for si_haptic input device
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- vibrator/Vibrator.cpp
+++ vibrator/Vibrator.cpp
@@ -112,7 +112,8 @@ InputFFDevice::InputFFDevice()
         }
 
         if (strcmp(name, "qcom-hv-haptics") && strcmp(name, "qti-haptics")
-            && strcmp(name, "aw8697_haptic")) {
+            && strcmp(name, "aw8697_haptic")
+            && strcmp(name, "si_haptic")) {
             ALOGD("not a qcom/qti haptics device\n");
             close(fd);
             continue;
