87a5274a306cfae6a37430c9fca8a9bc3c68b71c
claxten10 <claxten10@gmail.com>
2025-11-23 14:22:38 +0000

    malachite: Drop stub mtkpower service
    * Our camera stack doesn't depend on mtkpower, so we can safely remove it.
    
    test: camera
    Change-Id: Idc500bca46d98c4945163273b56119a426c79f13
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -347,8 +347,7 @@ PRODUCT_NO_BIONIC_PAGE_SIZE_MACRO := true
 # Power
 $(call soong_config_set,power_libperfmgr,mode_extension_lib, //$(DEVICE_PATH):libperfmgr-ext-xiaomi)
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
-    vendor.mediatek.hardware.mtkpower@1.2-service.stub
+    android.hardware.power-service.lineage-libperfmgr
 
 PRODUCT_PACKAGES += \
     init.mt6878.power.rc
