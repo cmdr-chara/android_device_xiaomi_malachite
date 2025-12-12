e206b245076dd79f9a6ec19c0b61aee3062a859b
claxten10 <claxten10@gmail.com>
2025-12-12 19:02:10 +0000

    malachite: Switch to AOSP remote submix
    * MediaTek's r_submix has a few modifications over the AOSP one, mainly
      PCM dumping, which we already disabled with [1] and some misc changes
      which we don't really need, therefore move to the AOSP module.
    
    [1]: https://github.com/mt6878-devs/android_device_xiaomi_malachite/commit/afca579115ec688294a45fd9fdfe1bbf5ab2df40
    
    Change-Id: Ie7ae303501d305dde5fe943187f45105be1e02b8
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -93,7 +93,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.primary.default \
-    audio.r_submix.default \
     audio_policy.stub \
     audio.usbv2.default
 
@@ -104,7 +103,13 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc) \
     frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration_7_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml \
-    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
+    frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+
+# Audio (Remote Submix)
+PRODUCT_PACKAGES += \
+    audio.r_submix.default:32
+
+PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Bluetooth
