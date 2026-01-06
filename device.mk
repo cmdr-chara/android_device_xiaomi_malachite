9d60362386f4e5efbc18f339f0159f22299f72b1
claxten10 <claxten10@gmail.com>
2026-01-06 01:28:17 +0000

    malachite: Kang EuiccGoogle from mustang
    * BD3A.251105.010.E1/14337626
    
    Change-Id: I0ede71d6b4b5df355c87de6275301da8de0048ba
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- device.mk
+++ device.mk
@@ -164,6 +164,12 @@ PRODUCT_PACKAGES += \
 # Dynamic Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# eUICC
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
+    $(DEVICE_PATH)/configs/permissions/privapp-permissions-euiccgoogle.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-euiccgoogle.xml \
+    $(DEVICE_PATH)/configs/permissions/default-permissions-euiccgoogle.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/default-permissions/default-permissions-euiccgoogle.xml
+
 # Fastboot
 PRODUCT_PACKAGES += \
     android.hardware.fastboot-service.example_recovery \
